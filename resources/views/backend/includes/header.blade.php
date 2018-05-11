87b1b373b6d2bde51eaeab15eec593810f02383b
Dennis Abatayo <insic@neseapl.com>
2018-05-12 00:29:16 +0800

    [update] update admin layout

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -11,46 +11,15 @@
         <li class="nav-item px-3">
             <a class="nav-link" href="{{ route('admin.dashboard') }}">{{ __('navs.frontend.dashboard') }}</a>
         </li>
-
-        @if (config('locale.status') && count(config('locale.languages')) > 1)
-            <li class="nav-item px-3 dropdown">
-                <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <span class="d-md-down-none">{{ __('menus.language-picker.language') }} ({{ strtoupper(app()->getLocale()) }})</span>
-                </a>
-
-                @include('includes.partials.lang')
-            </li>
-        @endif
     </ul>
 
     <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-info">0</span></a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-list"></i></a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#"><i class="icon-location-pin"></i></a>
-        </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                <img src="{{ $logged_in_user->picture }}" class="img-avatar" alt="{{ $logged_in_user->email }}">
                 <span class="d-md-down-none">{{ $logged_in_user->full_name }}</span>
             </a>
 
             <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-header text-center">
-                    <strong>Heading</strong>
-                </div>
-                <a class="dropdown-item" href="#"><i class="fas fa-link"></i> Link<span class="badge badge-primary">0</span></a>
-                <a class="dropdown-item" href="#"><i class="fas fa-link"></i> Link</a>
-
-                <div class="dropdown-header text-center">
-                    <strong>Heading</strong>
-                </div>
-                <a class="dropdown-item" href="#"><i class="fas fa-link"></i> Link</a>
-                <a class="dropdown-item" href="#"><i class="fas fa-link"></i> Link</a>
                 <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}"><i class="fas fa-lock"></i> {{ __('navs.general.logout') }}</a>
             </div>
         </li>
